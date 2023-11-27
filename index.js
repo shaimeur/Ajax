36628bdcfa1758a0ebac31ed5356344468aa1e1b
shaimeur <haimeursaad@gmail.com>
2023-11-27 20:45:04 +0100

    demo to how to fecth data using the XmlHTTPRequest (oldway) vs the Fetch API

---
+++ index.js
@@ -0,0 +1,38 @@
+ /********************************************
+        Ecrire ici le code permettant
+            1 - de récupérer tous les Posts, Users et Comments
+            2 - de convertir les trois réponses en tableaux :
+                * users
+                * posts
+                * comments
+            3 - de créer un nouveau tableau nommé allPosts sous la forme :
+                [
+                    {
+                        id : 1,
+                        userName : 'toto',
+                        title : 'un article',
+                        body : 'le corps de l\'article',
+                        published : true,
+                        comments : [{...}, ...]
+                    },
+                    ...
+                ]
+            4 - Créer les fonctions utilitaires suivantes :
+                    4-1 sortByUserName() qui permet de trier par utilsateur
+                    4-2 sortByPostTitle() qui permet de trier par le titre du Post
+                    4-3 getPagePosts(offset, size = 10) qui retourne un nouveau tableau extrait du tableau allPosts à partir de offset et contenant size éléments
+            5 - Ecrire la fonction showPosts() qui permettra d'afficher les 10 premiers posts
+            6 - Ecrire la fonction showComments(postId) qui permet d'afficher les commentaires
+            de le Post ayant l'id postId
+            7 - (facultatif) Ajouter une pagination
+            8 - ...
+            9 - ....
+            10 - .....
+        *********************************************/
+            const URL_USERS = "https://jsonplaceholder.typicode.com/users";
+            const URL_POSTS = "https://jsonplaceholder.typicode.com/posts";
+            const URL_COMMENTS = "https://jsonplaceholder.typicode.com/comments";
+            var offset = 0,
+              size = 10;
+
+
